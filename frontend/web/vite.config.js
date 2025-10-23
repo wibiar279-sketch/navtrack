0e01cee8e1c9750363e3107919d990916d0cc340
wibiar279-sketch <wibiar279@gmail.com>
2025-10-23 11:14:25 +0700

    Update vite.config.js

--- frontend/web/vite.config.js
+++ frontend/web/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(() => {
       host: "0.0.0.0",
       port: 3000,
       open: true,
-      allowedHosts: ["navtrackshared-production.up.railway.app"],
+      allowedHosts: ["navtrackweb-production.up.railway.app"],
     },
     resolve: {
       alias: {
